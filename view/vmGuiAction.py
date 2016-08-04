f511ec519eb7239e83e58ecf73bbf13ba8df77c2
chenzhifengla <chen1511@foxmail.com>
2016-08-04 09:48:26 +0800

    将控制器传入子界面，子界面数据收取尚未写完

--- view/vmGuiAction.py
+++ view/vmGuiAction.py
@@ -36,7 +36,7 @@ class VmGuiAction(Ui_mainWindow):
         self.childWindows = []
         self.uiController = UserInterfaceController()
 
-        #虚拟机树字典，item:index
+        #虚拟机树字典，item:index，用来确定tree上的item与stackedWidget上的page对应关系
         self.itemList = {}
 
         # 获取运行虚拟机列表
@@ -49,6 +49,7 @@ class VmGuiAction(Ui_mainWindow):
         """
         #对界面静态元素可通过调用父类方法添加
         #对需要动态添加的部分在此方法实现
+        # 添加事件响应
         :param MainWindow:
         :return:
         """
@@ -63,10 +64,19 @@ class VmGuiAction(Ui_mainWindow):
         #添加事件响应
         self.addAction()
 
+    def addTreeItem(self):
+        """
+        # 根据虚拟机列表动态添加到treeWidget上
+        :return:
+        """
+        for i, name in enumerate(self.vms):
+            item = QtGui.QTreeWidgetItem(self.treeWidget)
+            item.setText(0, name)
+            self.itemList[item] = i
 
     def addChildWindow(self):
         """
-        # 从控制器中获取运行虚拟机的列表
+        # 根据运行虚拟机的列表
         # 维护一个子界面的列表，并依次添加到主界面中
         :return:
         """
@@ -77,33 +87,27 @@ class VmGuiAction(Ui_mainWindow):
             childWnd = QtGui.QWidget()
             self.childWindows.append(childWnd)
 
+            #简单包装子界面
             childWndGenerator.setupUi(childWnd)
             # 把虚拟机名字传入
             childWndGenerator.setupName(name)
             # 把虚拟机类型列表传入
             childWndGenerator.setupTypes(self.vmTypes)
+            # 把控制器传入，供子界面上响应部分消息
+            childWndGenerator.setupController(self.uiController)
+
+            # 把子界面插入到stackedWidget中
             self.stackedWidget.insertWidget(-1, childWnd)
 
         self.stackedWidget.setCurrentIndex(0)
 
-    def addTreeItem(self):
-        """
-        # 调用函数获取虚拟机列表
-        # 动态添加到treeWidget上
-        :return:
-        """
-        for i, name in enumerate(self.vms):
-            item = QtGui.QTreeWidgetItem(self.treeWidget)
-            item.setText(0, name)
-            self.itemList[item] = i
-
-
     def addAction(self):
         """
-        #对界面添加事件响应
+        # 对主界面添加事件响应
         :param MainWindow:
         :return:
         """
+        # 当treeWidget点击切换时，调用方法来切换stackedWidget
         QtCore.QObject.connect(self.treeWidget, QtCore.SIGNAL(_fromUtf8("itemClicked(QTreeWidgetItem*,int)")),self.treeItemSelect)
 
     def treeItemSelect(self, item, index):
--- view/vmGuiConfAction.py
+++ view/vmGuiConfAction.py
@@ -13,16 +13,73 @@
 @contact:   chen1511@foxmail.com
 @version:   1.0-2016-07-29
 """
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from vmGuiConf import Ui_Form
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
 
 class VmGuiConfAction(Ui_Form):
 
     def setupUi(self, Form):
         super(VmGuiConfAction, self).setupUi(Form)
+        # 添加子界面上的消息响应
+        self.addAction()
+
+    def addAction(self):
+        # 保存按钮
+        QtCore.QObject.connect(self.pushButton_save, QtCore.SIGNAL(_fromUtf8("clicked()")), self.save)
+        # 清空按钮
+        QtCore.QObject.connect(self.pushButton_clear, QtCore.SIGNAL(_fromUtf8("clicked()")), self.clear)
+        # 执行按钮
+        QtCore.QObject.connect(self.pushButton_execute, QtCore.SIGNAL(_fromUtf8("clicked()")), self.execute)
+        # 中断按钮
+        QtCore.QObject.connect(self.pushButton_break, QtCore.SIGNAL(_fromUtf8("clicked()")), self.break_)
+
+    def save(self):
+        """
+        # 点击保存时调用此方法
+        # 将该页上配置保存
+        :return:
+        """
+        processesMonitor =
+        self.uiController.setVmsConfs(self.name,
+                                      sysType = self.comboBox_systype.currentText(),
+                                      isCheckRootkit = self.checkBox_rootkit.isChecked(),
+                                      rootkitPolicy = self.comboBox_rootkit_policy.currentText(),
+                                      ip = (self.spinBox_ip1.text(), self.spinBox_ip2.text(), self.spinBox_ip3.text(), self.spinBox_ip4.text()),
+                                      )
+
+    def clear(self):
+        """
+        # 点击清空时调用此方法
+        # 将该页上配置清空，并清空对应的配置文件
+        :return:
+        """
+
+    def execute(self):
+        """
+        # 点击执行时调用此方法
+        #
+        :return:
+        """
+
+    def break_(self):
+        """
+        #
+        :return:
+        """
+
 
     def setupName(self, name):
+        self.name = name
         self.label_vmname.setText(name)
 
     def setupTypes(self, types):
         self.comboBox_systype.addItems(types)
+
+    def setupController(self, uiController):
+        self.uiController = uiController
